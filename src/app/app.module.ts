80e474afd19e9c722f315fa7c134432a95fb6439
Ayman Adam Taleb <taleba@uci.edu>
2023-07-19 01:27:03 -0700

    topnav made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumePageComponent,
+    ProjectsPageComponent,
+    ContactPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
