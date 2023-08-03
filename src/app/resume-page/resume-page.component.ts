0e6986a13bbff71018ed6c22ecfbc8ca27a1ed8e
Ayman Adam Taleb <taleba@uci.edu>
2023-08-02 19:02:47 -0700

    resume download button

--- src/app/resume-page/resume-page.component.ts
+++ src/app/resume-page/resume-page.component.ts
@@ -12,6 +12,16 @@ export class ResumePageComponent {
 
   constructor() { }
 
+  downloadPDF(){
+    const link = document.createElement('a');
+    link.setAttribute('target','_blank');
+    link.setAttribute('href','assets/resumeMain.pdf');
+    link.setAttribute('download','AymanTalebResume.pdf');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+
   ngOnInit(): void {
   }
 }
