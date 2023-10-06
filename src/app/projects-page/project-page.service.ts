f19ce68d0986a3e3db3e5fec4d765293040929c7
aymantaleb <ayman1779@gmail.com>
2023-10-05 21:49:22 -0700

    added projects and contact form

---
+++ src/app/projects-page/project-page.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectPageService {
+
+  
+
+  constructor(private http: HttpClient) { }
+
+  getGithubRepos(username: string): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${username}/repos`);
+  }
+}
