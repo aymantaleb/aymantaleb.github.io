1a264e559a20490713e678e4a553326e4a7b018d
aymantaleb <ayman1779@gmail.com>
2023-11-18 18:55:03 -0800

    new build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    emailUrl: 'AySiteEmailHandler-env.eba-desp2fem.us-west-1.elasticbeanstalk.com '
+    emailUrl: 'AySiteEmailHandler-env.eba-desp2fem.us-west-1.elasticbeanstalk.com'
   };
 
   
