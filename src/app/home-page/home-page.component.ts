7f78ab50dd21752940c18463dbccc30fa05688cb
aymantaleb <ayman1779@gmail.com>
2025-03-13 20:37:19 -0700

    fixed about me text

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -12,6 +12,22 @@ export class HomePageComponent implements OnInit {
   clickCount: number = 0;
   headshotImg: string = "assets/headshot.png"
   monkeyImg: string = "assets/monkey.png"
+  homepageAboutMe: string = `My name is Ayman Taleb and I'm a passionate Computer Engineer. I
+          graduated from UC Irvine with a degree in Computer Engineering in 2023
+          and Im constantly striving to improve my skill set. Currently, I am
+          working as a Applications Engineer at Universal Electronics Inc. When I'm not
+          working, I love to engage in personal projects, tinker with electronic
+          gadgets, and code handy applications. I believe that personal projects
+          are an excellent way to further develop my skills and explore unique
+          ideas outside the constraints of my daily work.
+          <br />
+          <br />
+          If you're interested in seeing the projects I've worked on, I
+          encourage you to visit my GitHub profile or the Projects page above.
+          Feel free to reach out to me, using the Contact page, if any of my projects catch your attention
+          or if you have any questions. I'm always open to discussing
+          technology, potential collaborations, or any exciting ideas you might
+          have!`
 
   constructor() {}
 
